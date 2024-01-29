f75d0ef5eb11b55a60c4c413c2b4ee8185f04f87
J Arts <78762941+Arts-of-coding@users.noreply.github.com>
2024-01-29 15:31:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "REPO ${REPOSITORY}"
 
-#REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
-
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${REPOSITORY} --token ${ACCESS_TOKEN}
