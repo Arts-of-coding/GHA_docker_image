45d3c95715f332cc266b96340aaf8a51fd68b502
J Arts <78762941+Arts-of-coding@users.noreply.github.com>
2024-01-29 15:59:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 echo "REPO ${REPOSITORY}"
-
 cd /home/docker/actions-runner
-
 ./config.sh --url https://github.com/${REPOSITORY} --token ${ACCESS_TOKEN}
-
 cleanup() {
     echo "Removing runner..."
     ./config.sh remove --unattended --token ${ACCESS_TOKEN}
 }
-
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
-
 ./run.sh & wait $!
